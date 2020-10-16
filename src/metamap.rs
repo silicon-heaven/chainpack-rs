8b6ae9fe7a24e269765157fbf0926f0c102c74d5
Fanda Vacek <fanda.vacek@gmail.com>
2020-10-16 20:51:53 +0200

    RpcValue::to_xxx() -> &RpcValue

--- src/metamap.rs
+++ src/metamap.rs
@@ -50,41 +50,48 @@ impl MetaMap {
         MetaMap(Vec::new())
     }
 
-    pub fn insert<I>(&mut self, key: I, value: &RpcValue)
+    pub fn insert<I>(&mut self, key: I, value: RpcValue) -> &mut Self
         where I: IntoMetaKeyRef {
-        self.0.push(MetaKeyVal{key: key.to_metakeyref().to_metakey(), value: value.clone()});
+        let ix = self.find(&key);
+        match ix {
+            None => self.0.push(MetaKeyVal{key: key.to_metakeyref().to_metakey(), value }),
+            Some(ix) => self.0[ix].value = value,
+        }
+        self
     }
 
-    fn find<I>(&self, key: I) -> Option<&MetaKeyVal>
+    fn find<I>(&self, key: &I) -> Option<usize>
         where I: IntoMetaKeyRef {
+        let mut ix = 0;
         for kv in self.0.iter() {
             let mk = key.to_metakeyref();
             match &kv.key {
                 MetaKey::String(k1) => {
                     match &mk {
-                        MetaKeyRef::Str(k2) => if k1 == k2 {return Some(kv)}
+                        MetaKeyRef::Str(k2) => if k1 == k2 {return Some(ix)}
                         _ 		=> (),
                     }
                 },
                 MetaKey::Int(k1) => {
                     match &mk {
-                        MetaKeyRef::Int(k2) => if k1 == k2 {return Some(kv)}
+                        MetaKeyRef::Int(k2) => if k1 == k2 {return Some(ix)}
                         _ 		=> (),
                     }
                 },
             }
+            ix = ix + 1;
         }
         None
     }
 
-    pub fn value<'a, I>(&'a self, key: I, def_val: Option<&'a RpcValue>) -> Option<&'a RpcValue>
-        where I: IntoMetaKeyRef {
-        let kv = self.find(key);
-        match kv {
-            Some(val) => Some(&val.value),
-            None => def_val
-        }
-    }
+    // pub fn value<'a, I>(&'a self, key: I, def_val: Option<&'a RpcValue>) -> Option<&'a RpcValue>
+    //     where I: IntoMetaKeyRef {
+    //     let ix = self.find(&key);
+    //     match ix {
+    //         Some(ix) => Some(&self.0[ix].value),
+    //         None => def_val
+    //     }
+    // }
 }
 
 impl fmt::Debug for MetaMap {
@@ -97,9 +104,9 @@ impl Index<&str> for MetaMap {
     type Output = RpcValue;
 
     fn index(&self, key: &str) -> &'_ Self::Output {
-        let kv = self.find(key);
-        match kv {
-            Some(val) => &val.value,
+        let ix = self.find(&key);
+        match ix {
+            Some(ix) => &self.0[ix].value,
             None => panic!("Invalid MetaMap key '{}'", key),
         }
     }
@@ -109,9 +116,9 @@ impl Index<i32> for MetaMap {
     type Output = RpcValue;
 
     fn index(&self, key: i32) -> &'_ Self::Output {
-        let kv = self.find(key);
-        match kv {
-            Some(val) => &val.value,
+        let ix = self.find(&key);
+        match ix {
+            Some(ix) => &self.0[ix].value,
             None => panic!("Invalid MetaMap key '{}'", key),
         }
     }
@@ -121,27 +128,46 @@ impl Index<i32> for MetaMap {
 mod test {
     use crate::metamap::MetaMap;
     use crate::rpcvalue::RpcValue;
+    use std::collections::HashMap;
 
     #[test]
     fn metamap_insert() {
         let mut mm = MetaMap::new();
-        let rv = RpcValue::new(());
 
-        mm.insert(123, &rv);
-        assert_eq!(mm[123], rv);
-        let vv = mm.value(123, None);
-        println!("inserted and retrieved: {:?}", vv);
-        assert_eq!(vv, Some(&rv));
+        mm.insert(123, RpcValue::new(1.1));
+        assert_eq!(mm[123].to_double(), 1.1);
+        // let vv = mm.value(1234, Some(&RpcValue::new(123)));
+        // println!("inserted and retrieved: {:?}", vv);
+        // assert_eq!(123, vv.unwrap().to_i32().unwrap());
 
-        mm.insert("abc", &rv);
-        assert_eq!(mm["abc"], rv);
-        let vv = mm.value("abc", None);
-        println!("inserted and retrieved: {:?}", vv);
-        assert_eq!(vv, Some(&rv));
+        mm.insert("foo", RpcValue::new("bar")).insert(123, RpcValue::new("baz"));
+        assert_eq!(mm["foo"].to_str(), "bar");
+        // println!("val: {:?}", mm[123]);
+        assert_eq!(mm[123].to_str(), "baz");
 
-        println!("metamap: {:?}", mm);
+        let v1 = vec![RpcValue::new("foo"), RpcValue::new("bar"), RpcValue::new("baz")];
+        let v2 = v1.clone();
+        mm.insert("list", RpcValue::new(v1));
+        assert_eq!(mm["list"].to_list(), &v2);
+
+        let mut v1: HashMap<i32, RpcValue> = HashMap::new();
+        v1.insert(1, RpcValue::new("foo"));
+        v1.insert(2, RpcValue::new("bar"));
+        v1.insert(3, RpcValue::new("baz"));
+        let v2 = v1.clone();
+        mm.insert("imap", RpcValue::new(v1));
+        assert_eq!(mm["imap"].to_imap(), &v2);
+
+        let mut v1: HashMap<String, RpcValue> = HashMap::new();
+        v1.insert("a".to_string(), RpcValue::new("foo"));
+        v1.insert("b".to_string(), RpcValue::new("bar"));
+        v1.insert("c".to_string(), RpcValue::new("baz"));
+        let v2 = v1.clone();
+        mm.insert("map", RpcValue::new(v1));
+        assert_eq!(mm["map"].to_map(), &v2);
+
     }
-
+    /*
     #[test]
     #[should_panic]
     fn metamap_invalid_string_key() {
@@ -153,9 +179,8 @@ mod test {
     #[should_panic]
     fn metamap_invalid_int_key() {
         let mut mm = MetaMap::new();
-        let rv = RpcValue::new(());
-        mm.insert(123, &rv);
+        mm.insert(123, RpcValue::new(()));
         let _a = &mm[1234];
     }
-
+    */
 }
--- src/rpcvalue.rs
+++ src/rpcvalue.rs
@@ -1,49 +1,88 @@
 use crate::metamap::MetaMap;
 use std::fmt;
+use std::collections::HashMap;
+use lazy_static::lazy_static;
 
 // see https://github.com/rhysd/tinyjson/blob/master/src/json_value.rs
+
+
+const STR_REF: &str = "";
+// const LIST_REF: Vec<RpcValue> = Vec::new();
+
+//const MAP_REF: HashMap<String, RpcValue> = HashMap::new();
+//const IMAP_REF: HashMap<i32, RpcValue> = HashMap::new();
+lazy_static! {
+    static ref LIST_REF: Vec<RpcValue> = {
+        let v = Vec::new();
+        v
+    };
+    static ref MAP_REF: HashMap<String, RpcValue> = {
+        let m = HashMap::new();
+        m
+    };
+    static ref IMAP_REF: HashMap<i32, RpcValue> = {
+        let m = HashMap::new();
+        m
+    };
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
 	Null,
-	String(String),
 	Int(i64),
 	UInt(u64),
 	Double(f64),
 	Bool(bool),
-	// DateTime(DateTime),
-	List(Vec<RpcValue>),
-	// Map(HashMap<String, RpcValue>),
-	// IMap(HashMap<i32, RpcValue>),
+	DateTime(i64),
+	Bytes(Box<Vec<u8>>),
+	String(Box<String>),
+	List(Box<Vec<RpcValue>>),
+	Map(Box<HashMap<String, RpcValue>>),
+	IMap(Box<HashMap<i32, RpcValue>>),
 }
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct DateTime {}
 
-pub trait ToValue {
-	fn to_value(self) -> Value;
+pub trait FromValue {
+	fn from_value(self) -> Value;
 }
 
-impl ToValue for () { fn to_value(self) -> Value { Value::Null } }
-impl ToValue for &str { fn to_value(self) -> Value { Value::String(String::from(self)) } }
-impl ToValue for i32 { fn to_value(self) -> Value { Value::Int(self as i64) } }
-impl ToValue for usize { fn to_value(self) -> Value { Value::UInt(self as u64) } }
-impl ToValue for Vec<RpcValue> { fn to_value(self) -> Value { Value::List(self) } }
+impl FromValue for () { fn from_value(self) -> Value { Value::Null } }
+impl FromValue for &str { fn from_value(self) -> Value { Value::String(Box::new(String::from(self))) } }
+impl FromValue for i32 { fn from_value(self) -> Value { Value::Int(self as i64) } }
+impl FromValue for usize { fn from_value(self) -> Value { Value::UInt(self as u64) } }
 
-macro_rules! to_value {
+macro_rules! from_value {
     ($from:ty, $to:ident) => {
-		impl ToValue for $from {
-			fn to_value(self) -> Value {
+		impl FromValue for $from {
+			fn from_value(self) -> Value {
 				Value::$to(self)
 			}
 		}
     };
 }
 
-to_value!(i64, Int);
-to_value!(u64, UInt);
-to_value!(f64, Double);
-to_value!(bool, Bool);
+from_value!(bool, Bool);
+from_value!(i64, Int);
+from_value!(u64, UInt);
+from_value!(f64, Double);
+
+macro_rules! from_value_box {
+    ($from:ty, $to:ident) => {
+		impl FromValue for $from {
+			fn from_value(self) -> Value {
+				Value::$to(Box::new(self))
+			}
+		}
+    };
+}
+
+from_value_box!(String, String);
+from_value_box!(Vec<RpcValue>, List);
+from_value_box!(HashMap<String, RpcValue>, Map);
+from_value_box!(HashMap<i32, RpcValue>, IMap);
 
 #[derive(PartialEq, Clone)]
 pub struct RpcValue {
@@ -53,10 +92,47 @@ pub struct RpcValue {
 
 impl RpcValue {
 	pub fn new<I>(val: I) -> RpcValue
-	where I: ToValue {
+	where I: FromValue {
 		RpcValue {
 			meta: Box::new(MetaMap::new()),
-			value: val.to_value(),
+			value: val.from_value(),
+		}
+	}
+
+	pub fn to_i32(&self) -> i32 {
+		match &self.value {
+			Value::Int(d) => *d as i32,
+			_ => 0,
+		}
+	}
+	pub fn to_double(&self) -> f64 {
+		match &self.value {
+			Value::Double(d) => *d,
+			_ => 0.,
+		}
+	}
+	pub fn to_str(&self) -> &str {
+		match &self.value {
+			Value::String(s) => &s,
+			_ => STR_REF,
+		}
+	}
+	pub fn to_list(&self) -> &Vec<RpcValue> {
+		match &self.value {
+			Value::List(b) => &b,
+			_ => &LIST_REF,
+		}
+	}
+	pub fn to_map(&self) -> &HashMap<String, RpcValue> {
+		match &self.value {
+			Value::Map(b) => &b,
+			_ => &MAP_REF,
+		}
+	}
+	pub fn to_imap(&self) -> &HashMap<i32, RpcValue> {
+		match &self.value {
+			Value::IMap(b) => &b,
+			_ => &IMAP_REF,
 		}
 	}
 }
@@ -70,18 +146,53 @@ impl fmt::Debug for RpcValue {
 #[cfg(test)]
 mod test {
 	use crate::metamap::MetaMap;
-	use crate::rpcvalue::RpcValue;
+	use crate::rpcvalue::{RpcValue, Value};
+	use std::collections::HashMap;
+	use std::mem::size_of;
+
+	macro_rules! show_size {
+		(header) => (
+			println!("{:<22} {:>4}    ", "Type", "T");
+			println!("------------------------------");
+		);
+		($t:ty) => (
+			println!("{:<22} {:4}", stringify!($t), size_of::<$t>())
+		)
+	}
 
 	#[test]
-	fn rpcval_new() {
+	fn size() {
+		show_size!(header);
+		show_size!(usize);
+		show_size!(MetaMap);
+		show_size!(Box<MetaMap>);
+		show_size!(Option<MetaMap>);
+		show_size!(Option<Box<MetaMap>>);
+		show_size!(Value);
+		show_size!(Option<Value>);
+		show_size!(RpcValue);
+	}
+
+	#[test]
+	fn rpcval_new()
+	{
 		let _rv = RpcValue::new(true);
 		let _rv = RpcValue::new("foo");
+		let _rv = RpcValue::new("bar".to_string());
 		let _rv = RpcValue::new(123);
 		let _rv = RpcValue::new(12.3);
 		let _rv = RpcValue::new(vec![RpcValue::new(123), RpcValue::new("foo")]);
 
-	}
+		let mut m: HashMap<String, RpcValue> = HashMap::new();
+		m.insert("foo".to_string(), RpcValue::new(123));
+		m.insert("bar".to_string(), RpcValue::new("foo"));
+		let _rv = RpcValue::new(m);
 
+		let mut m: HashMap<i32, RpcValue> = HashMap::new();
+		m.insert(1, RpcValue::new(123));
+		m.insert(2, RpcValue::new("foo"));
+		let _rv = RpcValue::new(m);
+	}
 
 }
 
