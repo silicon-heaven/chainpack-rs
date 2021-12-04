f2b5879b24b8ce8306bb0736fa5e95ecad81f07f
Fanda Vacek <fanda.vacek@gmail.com>
2021-12-04 19:41:13 +0100

    RpcFrame moved from project shv-rs here

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
+pub use datetime::DateTime;
+pub use decimal::Decimal;
+pub use metamap::MetaMap;
+pub use reader::{Reader, ReadError, ReadResult};
+pub use rpcmessage::{RpcMessage, RpcMessageMetaTags};
+pub use rpcvalue::{Blob, RpcValue};
+pub use rpcvalue::Value;
+pub use writer::{Writer, WriteResult};
+
+pub use crate::chainpack::{ChainPackReader, ChainPackWriter};
+pub use crate::cpon::{CponReader, CponWriter};
+
 mod datetime;
 mod decimal;
 mod metamap;
 pub mod metamethod;
 pub mod rpcvalue;
 pub mod rpctype;
+pub mod rpcframe;
 pub mod rpcmessage;
 
 mod reader;
@@ -11,15 +24,5 @@ mod writer;
 mod cpon;
 mod chainpack;
 
-pub use rpcvalue::{RpcValue, Blob};
-pub use rpcmessage::{RpcMessage, RpcMessageMetaTags};
-pub use rpcvalue::{Value};
-pub use datetime::{DateTime};
-pub use decimal::{Decimal};
-pub use metamap::{MetaMap};
-pub use reader::{Reader, ReadResult, ReadError};
-pub use writer::{Writer, WriteResult};
-pub use crate::cpon::{CponReader, CponWriter};
-pub use crate::chainpack::{ChainPackReader, ChainPackWriter};
-
-
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Error>;
